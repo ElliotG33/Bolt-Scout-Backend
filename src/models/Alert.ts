f2bf7b6f81431f0775e61d1b38c1afa5276ea286
maharjan-umesh <spangelrocks@gmail.com>
2025-01-11 12:51:43 -0600

    alert model update

--- src/models/Alert.ts
+++ src/models/Alert.ts
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const AlertSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     email: { type: String, required: true },
     keywords: [{ type: String, required: true }],
+    antiKeywords: [{ type: String }],
     frequency: { type: Number, required: true }, // In hours
     active: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now },
@@ -13,4 +14,4 @@ const AlertSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export const Alert = mongoose.model("Alert", AlertSchema);
+export const Alert = mongoose.model('Alert', AlertSchema);
