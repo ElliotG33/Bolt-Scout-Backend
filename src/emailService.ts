ee5b73e4503eef076cd7c276f418ca8f41aa3f18
maharjan-umesh <spangelrocks@gmail.com>
2025-01-21 12:31:53 -0600

    email config changes

--- src/emailService.ts
+++ src/emailService.ts
@@ -17,12 +17,12 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
     return;
   }
 
-  const isLocalhost = APP_URL.toLowerCase().includes('localhost');
+  // const isLocalhost = APP_URL.toLowerCase().includes('localhost');
 
   const transporter = nodemailer.createTransport({
     host: SMTP_HOST,
     port: parseInt(SMTP_PORT || '587'),
-    secure: isLocalhost ? false : true,
+    secure: false,//isLocalhost ? false : true,
     auth: {
       user: SMTP_USER,
       pass: SMTP_PASS,
